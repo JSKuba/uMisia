c19c5d9141bf753dfa6f445a89e29732d35f8398
JSKuba <jakub1chmielewski@gmail.com>
2021-01-14 19:13:37 +0100

    rekrutacja page

---
+++ blocks/block-prostokaty.php
@@ -0,0 +1,15 @@
+<article class="prostokaty">
+    <h2><?php block_field("title") ?></h2>
+    <div class="wrapper">
+        <div class="rect left">
+            <?php block_field("left") ?>
+            <!-- Czesne w wysokości 130zł, w cenie czesnego zajęcia dodatkowe tj: rytmika, taniec, balet, Aikido, język angielski, język niemiecki, logopedia, Zuchy, programowanie (nie wszystkie zajęcia w każdej grupie wiekowej). -->
+        </div>
+        <div class="rect right">
+            <?php block_field("right") ?>
+            <!-- Opłaty za posiłki to 9 zł.
+            Honorujemy Kartę Dużej Rodziny.
+            Ulgi na rodzeństwo uczęszczające do naszego Przedszkola. -->
+        </div>
+    </div>
+</article>
---
+++ blocks/block-zdjecie-tekst-komunikat.php
@@ -0,0 +1,18 @@
+<article class="ztk">
+    <h3><?php block_field("title") ?></h3>
+    <div class="wrapper">
+        <div class="top">
+            <div class="left">
+                <?php block_field("left") ?>
+            </div>
+            <div class="right">
+                <?php block_field("right") ?>
+            </div>
+        </div>
+        <div class="bottom">
+            <div class="komunikat">
+                <?php block_field("communicate") ?>
+            </div>
+        </div>
+    </div>
+</article>
