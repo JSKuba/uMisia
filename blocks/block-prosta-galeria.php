7c242eab757f25bc2e90657633a2c2af82919bba
JSKuba <jakub1chmielewski@gmail.com>
2021-03-03 20:04:11 +0100

    added gallery

---
+++ blocks/block-prosta-galeria.php
@@ -0,0 +1,9 @@
+<div class="galeria">
+    <ul class="images-list">
+        <?php for ($i = 1; $i <= 4; $i += 1) { ?>
+            <li>
+                <img src="https://picsum.photos/120<?=$i?>/800">
+            </li>
+        <?php } ?>
+    </ul>
+</div>
