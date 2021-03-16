b5eb54aea89b7ef05e5651e0d156309857c6ed3d
JSKuba <jakub1chmielewski@gmail.com>
2021-03-16 21:57:14 +0100

    update styles

--- blocks/block-hero.php
+++ blocks/block-hero.php
@@ -4,7 +4,7 @@
       <h1><span id="h1-top"><?php block_field('title') ?></span> <span id="h1-bottom"><?php block_field('title-2') ?></span></h1>
       <figure class="hero-figure-mobile"><img class="style-svg" src="<?php block_field('image') ?>"/></figure>
       <p><?php block_field('description') ?></p>
-      <button id="action-button"><?php block_field('cta') ?></button>
+      <a id="action-button" href="#o-nas"><?php block_field('cta') ?></a>
     </div>
     <div class="hero-right">
       <figure class="hero-figure-desktop"><img class="style-svg" src="<?php block_field('image') ?>"/></figure>
